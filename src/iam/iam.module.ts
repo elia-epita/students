bbdddf897a4f7cf4a6a5696c598994bdf89847ab
Elia El Atram <eliaatram@outlook.com>
2025-06-07 16:33:04 +0200

    JWTs

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,9 +5,16 @@ import { AuthenticationController } from './authentication/authentication.contro
 import { AuthenticationService } from './authentication/authentication.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   providers: [
     {
       provide: HashingService, // abstract classes cannot be registered as a provider as it cannot be instantiated
