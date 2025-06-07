7b40a25a7a43f75e21bed6a7ccb68897b390f939
Elia El Atram <eliaatram@outlook.com>
2025-06-07 15:20:50 +0200

    init new project - add user & coffee resources + db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StudentsModule } from './students/students.module';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { CommonModule } from './common/common.module';
-import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.required(),
-        DATABASE_PORT: Joi.number().default(5432),
-      }),
-    }),
-    StudentsModule,
+    CoffeesModule,
+    UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT!,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // models will be loaded automatically,
-      synchronize: true, // entities will be synced with the database (must be disabled in production)
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
-    CommonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
-import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      /**
-       * auto transform payload DTO instances
-       * because when we receive requests with payloads, these payloads
-       * come over the network as plain JS objects. This is done by design
-       *
-       * check the instance of createStudentDto
-       */
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  app.useGlobalInterceptors(
-    new WrapResponseInterceptor(),
-    new TimeoutInterceptor(),
-  );
-
-  // Setting up Swagger document
-  const options = new DocumentBuilder()
-    .setTitle('Students')
-    .setDescription('Students application')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-
-  // SwaggerModule.setup('api', app, document);
-
-  app.use(
-    '/api',
-    apiReference({
-      content: document,
-    }),
-  );
-
   await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();
